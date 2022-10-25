b13e5a9847650e0553b402d9af33043beefecbe6
rgsmapola <99263713+rgsmapola@users.noreply.github.com>
2022-10-26 01:13:34 +0800

    Add files via upload

---
+++ Item.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ex05photonmapola;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Rebecca
+ */
+public class Item {
+    private String name;
+    private String type;
+    private double cost;
+    private static ArrayList<Item> completeItemList = new ArrayList();
+
+    public Item(String name, String type, double cost){
+        this.name = name;
+        this.type = type;
+        this.cost = cost;
+        completeItemList.add(this);
+    }
+    public String getName(){
+        return name;
+    }
+    public String getType(){
+        return type;
+    }
+    public double getCost(){
+        return cost;
+    }
+    public static Item getItem(int index){
+        return completeItemList.get(index);
+    }
+    
+    public static void printAllItems(){
+        for(Item i: completeItemList){
+            System.out.printf("Name: %s%nType: %s%nCost:%.2f%n", i.getName(), i.getType(), i.getCost());
+            }
+    }
+}
+
